1390774d86f666934770db7575e9791790460e8e
Michał Hernas <michal@hernas.pl>
2016-01-19 16:30:49 +0100

    New build

--- dist/angular-globalize-wrapper.js
+++ dist/angular-globalize-wrapper.js
@@ -28,16 +28,43 @@ glModule.provider('globalizeWrapper', function () {
         var mainLoaded = false, supplementalLoaded = false, messagesLoaded = false;
         var mainData = [], supplementalData = [], messagesData = {};
 
-        loadResources(
-            cldrBasePath + '/supplemental',
-            supplementalResources,
-            function (results) {
-                for (var i = 0; i < results.length; i++)
-                    supplementalData.push(results[i].data);
-                supplementalLoaded = true;
-                finishLoading();
+          $http.get(cldrBasePath + '/availableLocales.json').then(function(results) {
+            var availableLocales = results.data.availableLocales;
+
+            var allResources = [];
+            for(var i = 0; i < availableLocales.length; i++) {
+              var locale = availableLocales[i];
+              allResources = allResources.concat(mainResources.map(function(mr) {
+                return '/'+locale+mr;
+              }));
             }
-        );
+            loadResources(
+                cldrBasePath + '/main/',
+                allResources,
+                function (results) {
+                    mainLoaded = true;
+                    mainData = [];
+                    for (var i = 0; i < results.length; i++)
+                    {
+                        mainData.push(results[i].data);
+                    }
+
+                    loadResources(
+                        cldrBasePath + '/supplemental',
+                        supplementalResources,
+                        function (results) {
+                            for (var i = 0; i < results.length; i++)
+                            {
+                                supplementalData.push(results[i].data);
+                            }
+                            supplementalLoaded = true;
+                            finishLoading();
+                        }
+                    );
+                }
+            );
+
+          });
 
         function loadResources(basePath, resources, success) {
             var promises = [];
@@ -67,30 +94,8 @@ glModule.provider('globalizeWrapper', function () {
                 return;
             }
 
-            mainLoaded = false;
-            $http.get(cldrBasePath + '/availableLocales.json').then(function(results) {
-              var availableLocales = results.data.availableLocales;
+            finishLoading();
 
-              var allResources = [];
-              for(var i = 0; i < availableLocales.length; i++) {
-                var locale = availableLocales[i];
-                allResources = allResources.concat(mainResources.map(function(mr) {
-                  return '/'+locale+mr;
-                }));
-              }
-              loadResources(
-                  cldrBasePath + '/main/',
-                  allResources,
-                  function (results) {
-                      mainData = [];
-                      for (var i = 0; i < results.length; i++)
-                          mainData.push(results[i].data);
-                      mainLoaded = true;
-                      finishLoading();
-                  }
-              );
-
-            });
             if(l10nEnabled) {
                 messagesLoaded = false;
 
@@ -123,7 +128,7 @@ glModule.provider('globalizeWrapper', function () {
                         messages[currentLocale] = messagesData[currentLocale];
                         Globalize.loadMessages(messages);
                     }
-                    
+
                     instance = Globalize(currentLocale);
                 }
 
